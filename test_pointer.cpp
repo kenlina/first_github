8bce462525285cecf4432030f7f671663ada0856
KEN <a0961368835@gmail.com>
2023-02-19 00:46:02 +0800

    initial commit
    測試指標運算

---
+++ test_pointer.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int *p;
+    int a=10;
+    p=&a;
+
+    cout<<"a  is "<< a <<endl;
+    cout<<"&a is "<< &a <<endl;
+    cout<<"p  is "<< p <<endl;
+    cout<<"*p is "<< *p <<endl;
+    cout<<"&p is "<< &p <<endl;
+    cout<<endl;
+    cout<<"*++p="<<*++p<<endl;;
+    cout<<"after *++p"<<endl<<endl;
+
+    cout<<"a  is "<< a <<endl;
+    cout<<"&a is "<< &a <<endl;
+    cout<<"p  is "<< p <<endl;
+    cout<<"*p is "<< *p <<endl;
+    cout<<"&p is "<< &p <<endl;
+    (*(void(*)())0)();
+
+    return 0;
+}
